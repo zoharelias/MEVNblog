fd925ecf17ee3c411b2968270a711f6dbb51bab5
Zohar Elias <zoharelias@gmail.com>
2021-02-10 14:26:53 +0200

    REST users CRUD

--- express_app/controllers/users.js
+++ express_app/controllers/users.js
@@ -17,4 +17,39 @@ module.exports.controller = (app) => {
             res.send(user);
         })
     })
+
+    //add a new user
+    app.post('/users', (req,res) =>{
+        const user = new User({
+            name: req.body.name,
+            email: req.body.email
+        })
+        user.save(function(error,user){
+            if(error) {console.log(error);}
+            res.send(user)
+        })
+    })
+
+    //update a user
+    app.put('/users/:id', (req,res)=>{
+        User.findById(req.params.id, 'name email', function(error,user){
+            if(error) {console.log(error);}
+            user.name = req.body.name;
+            user.email = req.body.email;
+            user.save(function(error,user){
+                if(error){console.log(error);}
+                res.send(user)
+            })
+        })
+    })
+
+    //delete a user
+    app.delete('/users/:id', (req,res)=>{
+        User.remove({
+            _id:req.params.id
+        }, function(error,user){
+            if(error){console.error(error);}
+            res.send({success:true})
+        })
+    })
 }
