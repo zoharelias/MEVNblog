e82cc4d2e5c192af183f8ee01ed1b69516740a53
Zohar Elias <zoharelias@gmail.com>
2021-02-17 08:45:25 +0200

    Home

--- movie_rating_app/src/components/Home.vue
+++ movie_rating_app/src/components/Home.vue
@@ -4,7 +4,7 @@
       <v-card>
         <v-card-title primary-title>
           <div>
-            <div class="headline">The Termonator</div>
+            <div class="headline">The Terminator 1</div>
             <span class="grey--text">1984 . Science fiction / Action 2h 30m</span>
           </div>
         </v-card-title>
@@ -127,6 +127,30 @@
           using every serviceable naval and civilian vessel that could be found. At
           the end of this heroic mission, 330,000 French, British, Belgian and Dutch
           soldiers were safely evacuated.
+        </v-card-text>
+        <v-card-actions>
+          <v-btn flat color="purple">Rate this movie</v-btn>
+          <v-spacer></v-spacer>
+        </v-card-actions>
+      </v-card>
+    </v-flex>
+    <v-flex xs4>
+      <v-card>
+        <v-card-title primary-title>
+          <div>
+            <div class="headline">The Terminator 1</div>
+            <span class="grey--text">1984 . Science fiction / Action 2h 30m</span>
+          </div>
+        </v-card-title>
+        <v-card-text>
+          It's been nearly two years since Superman's (Henry Cavill)
+          colossal battle with Zod (Michael Shannon) devastated the city of
+          Metropolis. The loss of life and collateral damage left many feeling angry
+          and helpless, including crime-fighting billionaire Bruce Wayne (Ben
+          Affleck). Convinced that Superman is now a threat to humanity, Batman
+          embarks on a personal vendetta to end his reign on Earth, while the
+          conniving Lex Luthor (Jesse Eisenberg) launches his own crusade against the
+          Man of Steel.
         </v-card-text>
         <v-card-actions>
           <v-btn flat color="purple">Rate this movie</v-btn>
