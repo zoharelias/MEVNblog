53df8535081ba7f470a3e67ee5430f45b816edc7
Zohar Elias <zoharelias@gmail.com>
2021-04-20 15:41:17 +0300

    Login validation

--- movie_rating_app/controllers/users.js
+++ movie_rating_app/controllers/users.js
@@ -1,5 +1,12 @@
 const User = require('../models/User.js');
 
+const passportJWT = require('passport-jwt');
+const jwt = require('jsonwebtoken');
+
+const ExtractJWT = passportJWT.ExtractJWT;
+const jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJWT.fromAuthHeaderWithScheme('jwt');
+jwt.secretOrKey = 'thisisthesecretkey';
 
 
 module.exports.controller = (app) => {
@@ -21,6 +28,30 @@ module.exports.controller = (app) => {
             }
             res.send({ user });
         });
+    });
+
+    //login a user
+    app.post('/users/login', (req,res) => {
+        if(req.body/email && req.body.password) {
+            const email = req.body.email;
+            const password = req.body.password;
+            User.getUserByEmail(email, (err,user) => {
+                if(!user){
+                    res.status(404).json({message: 'user not exist'});
+                } else {
+                    User.comparePassword(password,user.password, (error,isMatch) =>{
+                        if(error) throw error;
+                        if(isMatch){
+                            const payload = { id: user.id };
+                            const token = jwt.sign(payload, jwtOptions.secretOrKey);
+                            res.json({ message: 'OK', token});
+                        } else {
+                            res.status(401).json({ message: 'password incorrect'});
+                        }
+                    });
+                }
+            });
+        }
     });
 };
 
